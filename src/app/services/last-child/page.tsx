c70882bf9c2e90e3f58244846898cb257ef5365f
heiyanquan <2792652898@qq.com>
2024-05-05 21:43:49 +0800

    🐞 fix: last-child

--- src/app/services/last-child/page.tsx
+++ src/app/services/last-child/page.tsx
@@ -10,10 +10,10 @@ import useESPage from "@/components/ESPage"
 const Page: FC = () => {
   const [productsList, setproductsList] = useState(
     new Array(4).fill({
-      title: "默克Supelco®_气相色谱柱_配件和吸附剂-默克生命科学",
-      desc: "请根据您的分析气相色谱应用浏览我们的气相色谱(GC)设备、柱、消耗品和配请根据您的分析气相色谱应用浏览我们的气相色谱(GC)设备、柱、消耗品和配",
+      title: '原料检验',
+      desc: '我们的原材料检验服务有助于确保生物制剂生产所用材料的质量和纯度。',
     })
-  )
+  );
   const [applicationList, setapplicationList] = useState(
     new Array(5).fill({
       title: "大分子HPLC_生物分子反相HPLC_高效液相色谱法-默克生命科学",
@@ -115,43 +115,63 @@ const Page: FC = () => {
   }
 
   return (
-    <div className="products-last-child-page-wrapper">
-      <h1 className="font-black text-2xl mb-4">分析进样针</h1>
-      <Flex align="center" gap={"middle"}>
-        <Image src={"/img/products/product-last-child1.avif"} alt="" width={290} height={180} />
-        <div className="text-base">
-          选择合适的进样针是确保进样口密封严密、仪器操作正常、进样量准确和长使用寿命的关键。我们的产品品牌根据与应用的相关性、仪器相容性和个人偏好提供进样针。从标准优质
-          <a className="green">Hamilton、S</a>GE和VICI® Precision Sampling
-          分析气体和液体进样针中进行选择。我们还可提供针对独特应用设计的进样针配件和特种进样针。
+    <div className='services-last-child-page-wrapper'>
+      <h1 className='font-black text-2xl mb-4'>ADC产品表征</h1>
+      <Flex align='center' gap={'middle'}>
+        <Image
+          src={'/img/services/product-last-child1.avif'}
+          alt=''
+          width={290}
+          height={180}
+        />
+        <div className='text-base'>
+          连接单克隆抗体且可递送高效活性药物成分（HPAPI）的抗体药物偶联物（ADC）本质上非常复杂，因此需要通过对生物疗法进行表征来探究其结构、结合性、纯度和效能。作为您的表征服务合作伙伴，我们可在ADC“裸”药和其药物偶联物中的生物成分方面提供专家指导，通过更安全、可靠和快速的测试，帮助您在关键阶段加快产品的质量决策。
         </div>
       </Flex>
       <Divider />
-      <h2 className="font-black text-xl mb-4">相关产品资源</h2>
+      <h2 className='font-black text-xl mb-4'>相关产品资源</h2>
       <div>
-        <a href="" className="block green text-base">
+        <a href='' className='block green text-base'>
           /CN/zh/technical-documents/protocol/analytical-chemistry/gas-chromatography/general-syringe-care
         </a>
-        <a href="" className="block green text-base">
+        <a href='' className='block green text-base'>
           活页：Hamilton® 药物分析工具
         </a>
-        <p className="text-base">准确的药物分析需要许多耗材。 详细了解 Hamilton® 耗材如何支持制药行业。</p>
+        <p className='text-base'>
+          准确的药物分析需要许多耗材。 详细了解 Hamilton®
+          耗材如何支持制药行业。
+        </p>
       </div>
       <Divider />
-      <h2 className="font-black text-xl mb-4">产品</h2>
-      <Flex gap={"middle"}>
+      <h2 className='font-black text-xl mb-4'>产品</h2>
+      <Flex gap={'middle'}>
         <Collapse
-          className="w-72"
+          className='w-72'
           items={CollapseItems}
-          defaultActiveKey={["1"]}
+          defaultActiveKey={['1']}
           onChange={(key: string | string[]) => {
-            console.log(key)
+            console.log(key);
           }}
         />
-        <div className="Collapse-right-content">
-          <Flex align="center" gap={"middle"} justify="space-between" className="mb-2">
-            <Flex align="center" gap={"small"}>
-              <HsAdminInput Search placeholder="按关键词筛选" className="w-72" />
-              <HsAdminSelect value={sortValue} options={sortOptions} allowClear={false} className="mr-8" />
+        <div className='Collapse-right-content'>
+          <Flex
+            align='center'
+            gap={'middle'}
+            justify='space-between'
+            className='mb-2'
+          >
+            <Flex align='center' gap={'small'}>
+              <HsAdminInput
+                Search
+                placeholder='按关键词筛选'
+                className='w-72'
+              />
+              <HsAdminSelect
+                value={sortValue}
+                options={sortOptions}
+                allowClear={false}
+                className='mr-8'
+              />
             </Flex>
             {ESPage}
           </Flex>
@@ -160,44 +180,49 @@ const Page: FC = () => {
             dataSource={dataSource}
             rowKey={(record: any) => record.articleNumber}
             rowSelection={{
-              type: "checkbox",
+              type: 'checkbox',
               ...rowSelection,
             }}
             pagination={false}
-            expandable={{ expandedRowRender, defaultExpandedRowKeys: ["0"], columnTitle: <span>展开</span>, columnWidth: 60 }}
+            expandable={{
+              expandedRowRender,
+              defaultExpandedRowKeys: ['0'],
+              columnTitle: <span>展开</span>,
+              columnWidth: 60,
+            }}
           />
         </div>
       </Flex>
       <Divider />
-      <h2 className="font-black text-xl mb-4">相关产品类别</h2>
+      <h2 className='font-black text-xl mb-4'>相关应用</h2>
       <List
         grid={{ gutter: 24, column: 4 }}
-        dataSource={productsList}
+        dataSource={applicationList}
         renderItem={(item) => (
           <List.Item>
-            <div className="products-list-item">
-              <h3 className="mt-4 text-base green">{item.title}</h3>
+            <div className='products-list-item'>
+              <h3 className='mt-4 text-base green'>{item.title}</h3>
               <p>{item.desc}</p>
             </div>
           </List.Item>
         )}
       />
       <Divider />
-      <h2 className="font-black text-xl mb-4">相关应用</h2>
+      <h2 className='font-black text-xl mb-4'>相关服务</h2>
       <List
         grid={{ gutter: 24, column: 4 }}
-        dataSource={applicationList}
+        dataSource={productsList}
         renderItem={(item) => (
           <List.Item>
-            <div className="products-list-item">
-              <h3 className="mt-4 text-base green">{item.title}</h3>
+            <div className='products-list-item'>
+              <h3 className='mt-4 text-base green'>{item.title}</h3>
               <p>{item.desc}</p>
             </div>
           </List.Item>
         )}
       />
     </div>
-  )
+  );
 }
 
 export default Page
